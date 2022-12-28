72b27f113294210a06e00b7126553eaded75b971
ahsouza <annibalhsouza@gmail.com>
2022-12-28 17:32:45 -0300

    add comandos

---
+++ cli/utils/help.js
@@ -0,0 +1,48 @@
+const menus = {
+  main: `
+    tamar [command] <options>
+
+    * Lista de comandos
+
+    create ............ criar novo website
+    add ............ adicionar um novo post
+    build ............ gerar projeto
+    serve ............ iniciar um servidor local
+    deploy ............ publicar seu website
+    pure ............ limpar cache
+    version ............ exibir versão
+    help ............... exibir manual tamar`,
+
+  build: `
+    tamar create <options>
+
+    --theme, -t ..... adicione um tema padrão
+    --deploy, -d ..... faça implantacão após ser o site ser gerado
+
+    `,
+  serve: `
+    tamar serve <options>
+
+    --port, -p ..... especifique a porta
+    --logs, -l ..... ative logs para o servidor
+    `,
+  deploy: `
+    tamar deploy <options>
+
+    --aws, -a ..... implantar site no Amazon Web Services
+    --gcloud, -gc ..... implantar site no Google Cloud
+    --github, -gh ..... implantar site no GitHub
+    --bwc, -b ..... implantar site no Barca Cloud
+    --heroku, -h ..... implantar site no Heroku
+    --vercel, -v ..... implantar site no Vercel
+    `,
+
+}
+
+module.exports = (args) => {
+  const subCmd = args._[0] === 'help'
+    ? args._[1]
+    : args._[0]
+
+  console.log(menus[subCmd] || menus.main)
+}
